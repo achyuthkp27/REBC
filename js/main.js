19314ca284236b03fea42eabf8ac55c1f51f54c6
Achyuth KP <kpachyuthz@gmail.com>
2023-02-26 08:36:02 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+(function ($) {
+  "use strict";
+  setTimeout(() => {
+    const box = document.getElementById("loader");
+    box.style.display = "block";
+  }, 6500);
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+    } else {
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-100px");
+    }
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    loop: true,
+    nav: false,
+    dots: true,
+    items: 1,
+    dotsData: true,
+  });
+})(jQuery);
